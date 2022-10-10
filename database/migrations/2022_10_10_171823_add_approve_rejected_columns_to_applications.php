965efc6a69544b55be6a53f3d7a654df57c9d53e
Adrián Avilés <adrian.aviles@swaplicado.com.mx>
2022-10-10 12:24:04 -0500

    agregado columnas fecha aprobado / rechazado en applications

---
+++ database/migrations/2022_10_10_171823_add_approve_rejected_columns_to_applications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApproveRejectedColumnsToApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->date('approvede_date_n')->nullable()->default(null)->after('type_incident_id');
+            $table->date('rejected_date_n')->nullable()->default(null)->after('approvede_date_n');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('approvede_date_n');
+            $table->dropColumn('rejected_date_n');
+        });
+    }
+}
