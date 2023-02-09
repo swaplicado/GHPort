323304b9e8aa1a9f470d2a0bc783a3ff02dcbfe9
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-02-09 09:59:25 -0600

    Manual delegaciones

--- resources/views/delegations/modal_delegationForm.blade.php
+++ resources/views/delegations/modal_delegationForm.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Nueva delegación</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Nueva delegación para colaborador</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
