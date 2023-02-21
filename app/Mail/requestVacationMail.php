05614c0dc8b425720cab7d3f93f4aaefde150977
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-02-21 17:17:22 -0600

    Correccion en email de solicitud de vacaciones

--- app/Mail/requestVacationMail.php
+++ app/Mail/requestVacationMail.php
@@ -36,8 +36,11 @@ class requestVacationMail extends Mailable
                             ->where('id_application', $this->idApplication)
                             ->first();
 
-        $application->start_date = dateUtils::formatDate($application->start_date, 'ddd D-M-Y');
-        $application->end_date = dateUtils::formatDate($application->end_date, 'ddd D-M-Y');
+        // $application->start_date = dateUtils::formatDate($application->start_date, 'ddd D-M-Y');
+        // $application->end_date = dateUtils::formatDate($application->end_date, 'ddd D-M-Y');
+        $application->start_date = dateUtils::formatDate($application->start_date, 'D/m/Y dddd');
+        $application->end_date = dateUtils::formatDate($application->end_date, 'D/m/Y dddd');
+        $this->returnDate = dateUtils::formatDate($this->returnDate, 'D/m/Y dddd');
 
         $employee = \DB::table('users')
                         ->where('id', $this->idEmployee)
@@ -45,7 +48,7 @@ class requestVacationMail extends Mailable
 
         $email = env('MAIL_FROM_ADDRESS');
         return $this->from($email)
-                        ->subject('Solicitud de vacaciones')
+                        ->subject('[PGH] Solicitud vacaciones '.$employee->short_name)
                         ->view('mails.requestVacationMail')
                         ->with('application', $application)
                         ->with('employee', $employee)
