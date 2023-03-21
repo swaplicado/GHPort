1ab07ce6eff602452d4739ecf1f8b02e5bb8c94b
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-03-21 17:15:20 -0600

    no guardar cache en navegador

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -8,9 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
-    <meta http-equiv="Pragma" content="no-cache"/>
-    <meta http-equiv="Expires" content="0"/>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0"/>
+    <meta http-equiv="pragma" content="no-cache"/>
 
     <title>Portal GH</title>
 
