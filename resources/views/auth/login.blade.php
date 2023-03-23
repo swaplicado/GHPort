c947362388dede95f2e52e1589064b1020aaf467
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-03-23 16:50:51 -0600

    placeholder login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">Usuario:</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input id="username" placeholder="nombre.apellido" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
