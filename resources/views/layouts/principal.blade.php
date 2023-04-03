9676f7d37d5e238b3d6f5280c25f5cb7688b781e
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-04-03 11:16:48 -0600

    favicon de aeth

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -16,6 +16,7 @@
     <title>Portal GH</title>
 
     <!-- Custom fonts for this template-->
+    <link rel="icon" href="img/aeth_logo.png" type="image/ico">
     <link href="{{ asset('principal/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
