255a5265f63f05fbde6f41b94593cd9d393a64ab
Cesar Iván Orozco Natividad <cesar.i.orozco.n@gmail.com>
2023-04-17 17:44:53 -0600

    migraciones para distintos tipos de incidencias
    migración que agrega cambios a tabla tipo  incidencias, migración que agrega sistemas con que se interactúa y migración para tabla pivote

---
+++ database/migrations/2023_04_17_166946_add_info_to_cat_incidence_tps.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInfoToCatIncidenceTps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cat_incidence_tps', function (Blueprint $table) {
+            $table->boolean('is_active')->default(0)->after('incidence_tp_name');
+            $table->boolean('need_auth')->default(0)->after('is_active');
+            $table->unsignedBigInteger('interact_system_id')->default(1)->after('need_auth');
+
+            $table->foreign('interact_system_id')->references('id_int_sys')->on('interact_systems');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cat_incidence_tps', function (Blueprint $table) {
+            $table->dropForeign(['interact_system_id']);
+            $table->dropColumn('interact_system_id');
+            $table->dropColumn('need_auth');
+            $table->dropColumn('is_active');
+        });
+    }
+}
---
+++ database/migrations/2023_04_17_175836_create_tp_incidents_pivot_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTpIncidentsPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tp_incidents_pivot', function (Blueprint $table) {
+            $table->bigIncrements('id_pivot');
+            $table->unsignedBigInteger('tp_incident_id');
+            $table->bigInteger('ext_tp_incident_id');
+            $table->bigInteger('ext_cl_incident_id');
+            $table->unsignedBigInteger('int_sys_id');
+            $table->boolean('is_deleted')->default(0);
+
+            $table->foreign('tp_incident_id')->references('id_incidence_tp')->on('cat_incidence_tps');
+            $table->foreign('interact_system_id')->references('id_int_sys')->on('interact_systems');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tp_incidents_pivot');
+    }
+}
