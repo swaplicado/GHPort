3fd6ed169f53a04f4503a311b5e9a40d86ec1b6d
Cesar Iván Orozco Natividad <cesar.i.orozco.n@gmail.com>
2023-04-18 11:41:14 -0600

    bug fix migraciones

--- database/migrations/2023_04_17_166946_add_info_to_cat_incidence_tps.php
+++ database/migrations/2023_04_17_166946_add_info_to_cat_incidence_tps.php
@@ -15,7 +15,6 @@ class AddInfoToCatIncidenceTps extends Migration
     {
         Schema::table('cat_incidence_tps', function (Blueprint $table) {
 
-            $table->boolean('is_active')->default(0)->after('incidence_tp_name');
             $table->boolean('need_auth')->default(0)->after('is_active');
             $table->unsignedBigInteger('interact_system_id')->default(1)->after('need_auth');
 
@@ -34,7 +33,6 @@ class AddInfoToCatIncidenceTps extends Migration
             $table->dropForeign(['interact_system_id']);
             $table->dropColumn('interact_system_id');
             $table->dropColumn('need_auth');
-            $table->dropColumn('is_active');
         });
     }
 }
