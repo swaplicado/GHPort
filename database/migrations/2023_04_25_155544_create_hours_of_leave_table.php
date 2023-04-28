ce8a03e3d6977909247062f8b66112ddf6739e8c
Cesar Iván Orozco Natividad <cesar.i.orozco.n@gmail.com>
2023-04-28 10:36:15 -0600

    migración horas de permiso

---
+++ database/migrations/2023_04_25_155544_create_hours_of_leave_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoursOfLeaveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours_leave', function (Blueprint $table) {
+            $table->bigIncrements('id_hours_leave');
+            $table->string('folio_n');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->date('date_send_n');
+            $table->integer('total_days');
+            $table->integer('tot_calendar_days');
+            $table->text('ldays');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('request_status_id');
+            $table->unsignedBigInteger('type_incident_id');
+            $table->unsignedBigInteger('user_apr_rej_id');
+            $table->date('approved_date_n');
+            $table->date('rejected_date_n');
+            $table->text('sup_comments_n');
+            $table->text('emp_comments_n');
+            $table->boolean('is_deleted');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('request_status_id')->references('id_applications_st')->on('sys_applications_sts');
+            $table->foreign('type_incident_id')->references('id_incidence_tp')->on('cat_incidence_tps');
+            $table->foreign('user_apr_rej_id')->references('id')->on('users');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours_of_leave');
+    }
+}
